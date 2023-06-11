690f01a1add1582f65d31f7233bc3cdc9f94bbd5
Andrea Girlando <andreagirlando06@gmail.com>
2023-06-11 21:59:46 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { AppRoutingModule } from 'src/app-routing.module';
 import { DashboardComponent } from '../dashboard/dashboard.component';
