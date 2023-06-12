c0fc437db9ea76878e90741bb1b0a4857ceb75a4
Andrea Girlando <104303199+Fawn25@users.noreply.github.com>
2023-06-12 09:40:55 +0200

    Update note.service.ts

--- src/services/note.service.ts
+++ src/services/note.service.ts
@@ -10,13 +10,13 @@ export class NoteService {
 
 
   public creaNota(body:any){
-    return this.http.post("http://todolistgirlando.somee.com/api/Note/creaNota",body)
+    return this.http.post("https://todolistgirlando.somee.com/api/Note/creaNota",body)
   }
   public getNote(){
-    return this.http.get("http://todolistgirlando.somee.com/api/Note/getNoteByUserID")
+    return this.http.get("https://todolistgirlando.somee.com/api/Note/getNoteByUserID")
   }
   public removeNota(id:number){
-    return this.http.delete("http://todolistgirlando.somee.com/api/Note/cancellaNota?id="+id)
+    return this.http.delete("https://todolistgirlando.somee.com/api/Note/cancellaNota?id="+id)
   }
 
 }
