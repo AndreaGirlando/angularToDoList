d1c116368649c31cfd29d416adde50965e953d49
Andrea Girlando <andreagirlando06@gmail.com>
2023-06-14 20:56:45 +0200

    TEST

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,14 +6,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+  link:string = "https://todolistgirlando.somee.com/"
+  //link:string = "https://localhost:7234"
 
   public login(username:string,password:string){
     const headers = { 'content-type': 'application/json' , 'Access-Control-Allow-Origin': '*'}
-    return this.http.post("https://todolistgirlando.somee.com/api/Auth/login",{username:username,password:password},{ 'headers': headers })
+    return this.http.post(this.link+"api/Auth/login",{username:username,password:password},{ 'headers': headers })
   }
   public register(username:string,password:string){
     const headers = { 'content-type': 'application/json' , 'Access-Control-Allow-Origin': '*'}
-    return this.http.post("https://todolistgirlando.somee.com/api/Auth/register",{username:username,password:password},{ 'headers': headers })
+    return this.http.post(this.link+"api/Auth/register",{username:username,password:password},{ 'headers': headers })
   }
   public getJwt(){
     if(this.isUserLoggedIn()){
